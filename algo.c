608385fd3551dcdc419a53858dce96232a90d9b6
ryanpatiency <ryanpatiency@gmail.com>
2018-03-30 17:47:32 +0800

    Implement merge sort

--- algo.c
+++ algo.c
@@ -3,30 +3,61 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define UP 100000
 
+void __merge_sort(int a[], int new_a[], int start, int end)
+{
+    if(start == end)
+        return;
+    int mid = (start + end) / 2;
+    __merge_sort(a, new_a, start, mid);
+    __merge_sort(a, new_a, mid + 1, end);
 
+    int tmp = start;
+    int i = start;
+    int j = mid + 1;
+    while(i <= mid && j <= end) {
+        new_a[tmp++] = a[i] > a[j] ? a[j++] : a[i++];
+    }
+    if(i > mid)
+        while(j <= end)
+            new_a[tmp++] = a[j++];
+    if(j > end)
+        while(i <= mid)
+            new_a[tmp++] = a[i++];
+    memcpy(a + start, new_a + start, (1 + end - start) * sizeof(int));
+}
 
-int main()
+void merge_sort(int *a, int size)
 {
     /*
-     * from second to last, sort key in to the sorted array, if the key is 
-     * smaller than the front one, copy front one to back and compare to the 
-     * one front to front one, until there is no front one, or front one is 
-     * smaller than key.
-     * 
-     * Then, we copy insert the key into this location
+     * Divide:
+     *  n to n/2
+     * Conquer
+     *  if n==1, return
+     * Combine
+     *  for two subarray, put the small element first
      */
-    
-    int a[] = {5, 4, 3, 2, 1};
-    for(int i = 1; i < 5; i++) {
+    int *new_a = malloc(size * sizeof(int));
+    __merge_sort(a, new_a, 0, size - 1);
+    free(new_a);
+}
+
+void insert_sort(int a[], int size)
+{
+    for(int i = 1; i < size; i++) {
         int key = a[i];
         int j;
         for(j = i - 1; j >= 0 && key < a[j]; j--) {
             a[j + 1] = a[j];
         }
-        a[j + 1] = key;    
+        a[j + 1] = key;
     }
+}
+
+int main()
+{
+    int a[] = {5, 4, 3, 2, 1};
+    merge_sort(a, 5);
     for(int i = 0; i < 5; i++) {
         printf("%d ", a[i]);
     }
