95ea6f8232c60c2cd05ac26d27e18c4da4ba3f81
ryanpatiency <ryanpatiency@gmail.com>
2018-04-03 13:38:39 +0800

    Use basic multiply with the same argument

--- matrix/basic_multiply.c
+++ matrix/basic_multiply.c
@@ -1,11 +1,16 @@
 void basic_multiply(int *a, int *b, int *c, int size)
 {
+	int *_c = malloc(sizeof(int) * size * size);
 	for(int row = 0; row < size; row++) {
 		for(int col = 0; col < size; col++) {
 			for(int k = 0; k < size; k++) {
-				*(c + row * size + col) = *(a + row * size + k)
+				*(_c + row * size + col) = *(a + row * size + k)
 				        * (*(b + k * size + col));
 			}
 		}
 	}
+	for(int i = 0; i < size * size; i++) {
+		c[i] = _c[i];
+	}
+	free(_c);
 }
